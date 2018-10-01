ab0832739f701edd5d7d2d5691a2e2211e302c37
Chris Pestano <pestano@calpoly.edu>
2018-09-30 20:54:50 -0700

    Added start of bounds tests

--- proj1/SortTest.java
+++ proj1/SortTest.java
@@ -6,8 +6,24 @@ import org.junit.Test;
 
 public class SortTest
 {
+    MyClass tester;
+
    //test uniform arrays (just print)
+
    //test bounds
+   @test
+   public void testBounds(){
+      final int k = 1000;
+      int[] arr1 = new int[k];
+      int[] arr2 = new int[k];
+      int[] arr3 = new int[k];
+      SortTimes.randInit(arr1, arr2, arr3, k);
+      for (int i = 0; i < k; i++){
+         assertTrue(arr1[i] < k, "Upperbound broken");
+         assertTrue(arr1[i] > 0, "Lowerbound broken");
+         
+      }
+   }
 
    //test order
 }
