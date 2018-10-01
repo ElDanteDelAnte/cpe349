8fb784a8bad1daccf737da565fae79dfe42e1579
Chris Pestano <pestano@calpoly.edu>
2018-10-01 11:59:39 -0700

    Started SelectionSort

--- proj1/Sorts.java
+++ proj1/Sorts.java
@@ -2,7 +2,17 @@ public class Sorts
 {
    public static void selectionSort(int[] arr, int N)
    {
+      int minPos, tempVal;
       
+      for(int i = 0; i < N - 1; i++){
+         minPos = i;
+         for(int j = i; j < N - 1; j++){
+            if(arr[j] < arr[minPos]){
+               minPos = 
+            }    
+         }    
+
+      }    
    }
 
    public static void mergeSort(int[] arr, int N)
