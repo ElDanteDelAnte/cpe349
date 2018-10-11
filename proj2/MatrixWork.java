0ccf27d8318b4b23f0974bd35e06b17edd979c93
Chris Pestano <pestano@calpoly.edu>
2018-10-11 07:31:33 -0700

    Pseudocode implemented

---
+++ proj2/MatrixWork.java
@@ -0,0 +1,37 @@
+import Java.util.Scanner;
+import java.io.*; 
+public class MatrixWork
+
+   public static int[][] matrixProduct(int[][] A int[][] B){
+      //try catch block? throw IllegalArgumentException if A[0].length != B.length
+      //n = A.length
+      //k = B.length
+      //m = B[1].length
+      //DefineC[1..n, 1..m], initialize all cells with 0 // C has n rows and m columns
+      //for i = 1 to n
+      // forj = 1 to m
+      //    for l = 1 to k 
+      //       C[i, j] = C[i, j] + A[i,l] * B[l, j]
+      //return C;
+   }
+
+
+   public static void main(String[] args){
+      //initialize scanner for user i/o and another for file reading
+      //prompt user for input and scan user Input
+      //close user input scanner?
+      //Open file
+      //scan two ints as sizes for first  array
+      //create 2d array given scanned values
+      //create nested for loop where they terminate at array dimensions?
+      //scan file and fill out array as needed. (nextInt()?)
+      //scan two ints as sizes for second array
+      //create 2d array given scanned values 
+      //create nested for loop...
+      //scan file and fill out array as needed
+      //close file scanner?
+      //call matrixProduct
+      //output results (nested forloop of prints?)
+      //println after inner forloop to start next row in 2d array
+   }
+}
