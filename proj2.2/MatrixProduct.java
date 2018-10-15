b323f5f20a4a85c018a091b741866e3db3bb5870
ElDanteDelAnte <sreddell@att.net>
2018-10-15 11:45:46 -0700

    MatrixProduct skeleton and tests.

---
+++ proj2.2/MatrixProduct.java
@@ -0,0 +1,18 @@
+/**
+ * A class for multiplying matricies via recursion.
+ *
+ * @author Sean Reddell sreddell
+ * @author Chris Pestano pestano
+ * @version 10/15/18
+ */
+
+public class MatrixProduct
+{
+   public static int[][] matrixProduct_DAC(int[][] A, int[][] B)
+   {
+   }
+
+   public static int[][] matrixProduct_Strassen(int[][] A, int[][] B)
+   {
+   }
+}
