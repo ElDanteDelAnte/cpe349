457be8b2167eeed1090915271dcda8d059702c5d
Chris Pestano <pestano@calpoly.edu>
2018-10-17 18:49:12 -0700

    Standard DAC implemented

--- proj2.2/MatrixProduct.java
+++ proj2.2/MatrixProduct.java
@@ -8,6 +8,26 @@
 
 public class MatrixProduct
 {
+   private static int[][] addMatrices(int[][] product1, int[][] product2){
+      int n = product1.length;
+      int[][] C = new int[n][n];
+      for (int i = 0; i < n; i++){
+          for(int j = 0; j < n; j++){
+             C[i][j] = product1[i][j] + product2[i][j];
+          }    
+      }
+      return C;
+   }
+   private static int[][] subMatrices(int[][] product1, int[][] product2){
+      int n = product1.length;
+      int[][] C = new int[n][n];
+      for (int i = 0; i < n; i++){
+          for(int j = 0; j < n; j++){
+             C[i][j] = product1[i][j] - product2[i][j];
+          }    
+      }
+      return C;
+   }
    private static void validityCheck(int[][] A, int[][] B) //throws IllegalArgumentException
    {
       int n = A.length;
@@ -24,10 +44,78 @@ public class MatrixProduct
    public static int[][] matrixProduct_DAC(int[][] A, int[][] B)
    {
       validityCheck(A, B);
-      int[][] product = new int[A.length][A.length];
-      return product;
+      return matrixProduct_DAC(A, 0, 0, B, 0, 0, A.length);
    }
 
+   private static int[][] matrixProduct_DAC(int[][] A, int startrowA, int startcolA,
+                                            int[][] B, int startrowB, int startcolB, int n){
+      int[][] C = new int[n][n];
+      //Base case
+      if (n == 1){
+          C[0][0]= A[startrowA][startcolA] * B[startrowB][startcolB];
+      }
+      //Get quadrants of product matrix
+      else{
+          int[][] C11, C12, C21, C22;
+          
+          C11 = addMatrices(matrixProduct_DAC(A, startrowA, startcolA, 
+                                              B, startrowB, startcolB, 
+                                              n/2),
+                            matrixProduct_DAC(A, startrowA, startcolA + (n/2),
+                                              B, startrowB + (n/2), startcolB,
+                                              n/2));
+
+          C12 = addMatrices(matrixProduct_DAC(A, startrowA, startcolA,
+                                              B, startrowB, startcolB + (n/2),
+                                              n/2),
+                            matrixProduct_DAC(A, startrowA, startcolA + (n/2),
+                                              B, startrowB + (n/2), startcolB + (n/2), 
+                                              n/2));
+
+          C21 = addMatrices(matrixProduct_DAC(A, startrowA + (n/2), startcolA,
+                                              B, startrowB, startcolB, 
+                                              n/2),
+                            matrixProduct_DAC(A, startrowA + (n/2), startcolA + (n/2),
+                                              B, startrowB + (n/2), startcolB, 
+                                              n/2));
+
+          C22 = addMatrices(matrixProduct_DAC(A, startrowA + (n/2), startcolA,
+                                              B, startrowB, startcolB + (n/2),
+                                              n/2),
+                            matrixProduct_DAC(A, startrowA + (n/2), startcolA + (n/2),
+                                              B, startrowB + (n/2), startcolB + (n/2), 
+                                              n/2));
+          //Fill product matrix by quadrant
+          //C11
+          for (int i = 0; i < n/2; i++){
+              for (int j = 0; j < n/2; j++){
+                  C[i][j] = C11[i][j];
+              }
+          }
+          
+          //C12
+          for (int i = 0; i < n/2; i++){
+              for (int j = 0; j < n/2; j++){
+                  C[i][j + n/2] = C12[i][j];
+              }
+          }
+
+          //C21
+          for (int i = 0; i < n/2; i++){
+              for (int j = 0; j < n/2; j++){
+                  C[i + n/2][j] = C21[i][j];
+              }
+          }
+
+          //C22
+          for (int i = 0; i < n/2; i++){
+              for (int j = 0; j < n/2; j++){
+                  C[i + n/2][j + n/2] = C22[i][j];
+              }
+          }
+       }
+       return C;
+   }
    public static int[][] matrixProduct_Strassen(int[][] A, int[][] B)
    {
       validityCheck(A, B);
