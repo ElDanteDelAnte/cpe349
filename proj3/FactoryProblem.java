137fb96d8c7e3846a380c5ffdd51f152ae15252a
ElDanteDelAnte <sreddell@att.net>
2018-10-30 17:51:56 -0700

    Project 3 part 1 I/O and pseudocode

--- proj3/FactoryProblem.java
+++ proj3/FactoryProblem.java
@@ -11,6 +11,45 @@ import java.util.Scanner;
 
 public class FactoryProblem
 {
+   private class FactorySolution
+   {
+      private int[] f1, f2, l1, l2;
+      private int lastLine;
+
+      public FactorySolution(int[] f1, int[] f2, int[] l1, int[] l2, int lastLine)
+      {
+         this.f1 = f1;
+         this.f2 = f2;
+         this.l1 = l1;
+         this.l2 = l2;
+         this.lastLine = lastLine;
+      }
+
+      //print method
+      public void printSolution()
+      {
+         //for n = length; n > 0; n--
+      }
+   }
+
+   //actual algorithm
+   public static FactorySolution Compute(int[] a1, int[] a2, int[] t1, int[] t2,
+                                         int e1, int e2, int x1, int x2, int n)
+   {
+      //add entry and exit times to first and last stations? (might help)
+      //define time table and line table
+      //for i = 0 to n
+         //find min of each fork from top
+            //add transfer to next station across
+            //compare with next station
+         //find min of each fork from bottom
+            //add transfer to next station across
+            //compare with next station
+         //record each min and line
+
+      //create and return result
+   }
+
    public static void main(String[] args)
    {
       Scanner in = new Scanner(System.in);
@@ -56,16 +95,18 @@ public class FactoryProblem
          
          //switch from line 2
          t2 = new int[n - 1];
-            t1[i] = fileReader.nextInt();
+         for (int i = 0; i < n - 1; i++)
+            t2[i] = fileReader.nextInt();
 
-         //add entry and exit times to first and last stations? (might help)
 
          //actually run the method
+
+         //output result
       }
       catch (IOException e)
       {
          System.out.println(e.getMessage());
       }
-      //output result
+
    }
 }
