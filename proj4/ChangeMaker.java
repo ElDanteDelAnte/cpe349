15afa7853fb00d366fb8d977c938c9b8e4b9f73c
ElDanteDelAnte <sreddell@att.net>
2018-11-14 15:13:21 -0800

    Formatting discrepency on Proj4

--- proj4/ChangeMaker.java
+++ proj4/ChangeMaker.java
@@ -6,14 +6,111 @@
  * @version 11/7/18
  */
 
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class ChangeMaker
 {
    public static int[] change_DP(int n, int[] d)
    {
+      /* Definitions */
+      int[] count = new int[d.length];       //array B
+      int[] minCoins = new int[n + 1];       //array C
+      int[] coinTaken = new int[n + 1];      //array A
+      int j;
+
+      minCoins[0] = 0;  //base case
+
+      /* Solve sub-problems */
+      //fill out minCoins
+      for (j = 1; j <= n; j++)
+      {
+         int currMin = n;
+
+         //find best coin
+         for (int i = 0; i < d.length; i++)
+         {
+            //take coin
+            if ((j >= d[i]) && (minCoins[j - d[i]] < currMin))
+            {
+               //System.out.print(minCoins[j - d[i]] + " ");
+               currMin = minCoins[j - d[i]];
+               coinTaken[j] = i;
+            }
+         }
+         //System.out.print(currMin + " ");
+
+         minCoins[j] = currMin + 1;
+      }
+
+      /* Construct optimal solution */
+      j = n;
+      while (j > 0)
+      {
+         int coinType = coinTaken[j];
+         count[coinType]++;
+         j -= d[coinType];
+      }
+
+      //TESTING
+      //System.out.println(Arrays.toString(coinTaken));
+      //System.out.println(Arrays.toString(minCoins));
+      //System.out.println(Arrays.toString(count));
+      return count;
+   }
+   
+   //print results of the DP algorithm
+   private static void printCoins(int n, int[] d, int[] counts)
+   {
+      System.out.println("Amount: " + n);
+      
+      //print each coin
+      System.out.print("Optimal distribution: ");
+      int c = 0;
+      boolean hasPrinted = false;
+      for (int i = 0; i < d.length; i++)
+      {
+         //print coin count and size
+         if (counts[i] > 0)
+         {
+            //print + if needed
+            if (hasPrinted)
+               System.out.print(" + ");
+
+            hasPrinted = true;
+            System.out.print(counts[i] + "*" + d[i] + "c");
+         }
+
+         //tally total
+         c += counts[i];
+      }
+
+      System.out.println();
+      System.out.println("Optimal coin count: " + c);
+   }
+
+   public static int[] change_greedy(int n, int[] d)
+   {
+      int[] count = new int[d.length];
+      int j = n;
+
+      //iterate across coin types
+      for (int i = 0; i < d.length; i++)
+      {
+         //take as many as you can
+         while (j >= d[i])
+         {
+            j -= d[i];
+            count[i]++;
+         }
+      }
+
+      return count;
    }
 
    public static void main(String[] args)
    {
+      /* Receive input */
       Scanner input = new Scanner(System.in);
       System.out.println("Number of coin types: ");
       int k = input.nextInt();
@@ -22,12 +119,26 @@ public class ChangeMaker
       for (int i = 0; i < k; i++)
          d[i] = input.nextInt();
 
-      System.out.println("Value for n: ");
+      System.out.println("Enter a positive amount to be changed (enter 0 to quit):");
 
       int n = input.nextInt(); 
       while (n > 0)
       {
+         //Dynamic Programing
          int[] change = change_DP(n, d);
+         System.out.println("DP algorithm results");
+         printCoins(n, d, change);
+
+         //Greedy Algorithm
+         change = change_greedy(n, d);
+         System.out.println("Greedy algorithm results");
+         printCoins(n, d, change);
+         
+         //next input
+         System.out.println("Enter a positive amount to be changed (enter 0 to quit):");
+         n = input.nextInt();
       }
+
+      System.out.println("Thanks for playing. Good Bye.");
    }
 }
---
+++ proj4/Tester.java
@@ -0,0 +1,61 @@
+/**
+ * Project 4.
+ *
+ * Tester class.
+ * @author Sean Reddell sreddell
+ * @author Chris Pestano pestano
+ * @version 11/14/18
+ */
+
+public class Tester
+{
+   private static boolean match(int[] dp, int[] greedy)
+   {
+      if (dp.length != greedy.length)
+         return false;
+
+      for (int i = 0; i < dp.length; i++)
+         if (dp[i] != greedy[i])
+            return false;
+             
+      return true;
+   }
+
+   private static int test(int[] d)
+   {
+      int matches = 0;
+
+      for (int i = 1; i <= 200; i++)
+      {
+         int[] dp = ChangeMaker.change_DP(i, d);
+         int[] greedy = ChangeMaker.change_greedy(i, d);
+         if (match(dp, greedy))
+            matches++;
+      }
+
+      return matches;
+   }
+
+   public static void main(String[] args)
+   {
+      int[] US = {100, 50, 25, 10, 5, 1};
+      int[] SOVIET = {100, 50, 20, 15, 10, 5, 3, 2, 1};
+      int[] POW2 = {64, 32, 16, 8, 4, 2, 1};
+      int[] SANSNICK = {100, 50, 25, 10, 1};
+      int[] WEIRD = {66, 35, 27, 18, 10, 1};
+      
+      int[][] sets = new int[5][];
+      sets[0] = US;
+      sets[1] = SOVIET;
+      sets[2] = POW2;
+      sets[3] = SANSNICK;
+      sets[4] = WEIRD;
+
+      System.out.println("Testing change_DP and change_greedy algortihms");
+      for (int i = 0; i < sets.length; i++)
+      {
+         int matches = test(sets[i]);
+         System.out.println("Testing set" + (i + 1) + ": " + matches + " in 200 tests");
+      }
+   }
+}
